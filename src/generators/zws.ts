ad3ccb28467afc72c7ca976252e49acc833280d8
Izac Lorimer <izac@gordhoard.org>
2019-12-14 01:50:46 -0900

    Fix up some zero widths

--- src/generators/zws.ts
+++ src/generators/zws.ts
@@ -1,14 +1,14 @@
 import { randomBytes } from 'crypto'
 
 const zwChars = [
-  '\u200b', // Zero-width space
-  '\u200c', // Zero-width non-joiner
-  '\u2060', // Zero-width word joiner
-  '\ufeff'  // Zero-width no-break space
+  '\u200B',
+  '\u200D',
+  '\u2060',
+  '\u180E'
 ]
 
 // Cap every link with this so it won't get trimmed out of messages
-const zwc = '\u200b'
+const zwc = '\u200B'
 
 export const zws = () => {
   return 'owo.vc/' + [...randomBytes(32)].reduce((prev, curr) => prev + zwChars[curr % zwChars.length], '') + zwc
