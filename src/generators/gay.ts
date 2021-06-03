aae138a556cb4c9840db0842732aaf7967da0d59
Katlyn Lorimer <katlyn@katlyn.dev>
2021-06-03 01:47:29 -0600

    feat: add gay generator

---
+++ src/generators/gay.ts
@@ -0,0 +1,36 @@
+import { randomBytes } from 'crypto'
+
+const gays = [
+  'LGBT', 'lgbt',
+  'gay', 'homosexual',
+  'lesbian',
+  'bi', 'bisexual',
+  'ace', 'asexual',
+  'aro', 'aromantic',
+  'trans', 'transgender', 'enby', 'nonbinary',
+  'pan', 'pansexual',
+  'poly',
+  'proud'
+]
+
+const subdomains = [...new Set(gays.map(w => w.toLocaleLowerCase()))]
+
+const dividers = ['-', '_', '/', '.']
+
+export const gay = (): string => {
+  const random = [...randomBytes(12)]
+  let id = subdomains[+random[0] % subdomains.length]
+  id += '.owo.gay/'
+  id += gays[+random[1] % gays.length]
+  id += dividers[+random[2] % dividers.length]
+  id += gays[+random[3] % gays.length]
+  id += dividers[+random[4] % dividers.length]
+  id += gays[+random[5] % gays.length]
+  id += dividers[+random[6] % dividers.length]
+  id += gays[+random[7] % gays.length]
+  id += dividers[+random[8] % dividers.length]
+  id += gays[+random[9] % gays.length]
+  id += dividers[+random[10] % dividers.length]
+  id += gays[+random[11] % gays.length]
+  return id
+}
