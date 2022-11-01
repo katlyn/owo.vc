ab76abab2ce9367c4af724d5a85debd8a8994367
Vendicated <vendicated@riseup.net>
2022-11-01 17:55:32 +0100

    yeet semi

--- src/index.ts
+++ src/index.ts
@@ -267,7 +267,7 @@ app.use(async (req, res) => {
   res.status(404).send('404 Not Found u-u')
 })
 
-const port = Number.isNaN(process.env.PORT) ? 80 : Number(process.env.PORT);
+const port = Number.isNaN(process.env.PORT) ? 80 : Number(process.env.PORT)
 app.listen(port).on('close', async () => {
   await prisma.$disconnect()
 })
