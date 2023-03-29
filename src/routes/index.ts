e1c0d4944e61663e423204202f762589ab0eddeb
Katlyn Lorimer <katlyn@katlyn.dev>
2023-03-28 22:31:21 -0800

    feat: update scrape and visit counts in database

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { fastifyStatic } from "@fastify/static"
+import { LinkStatus } from "@prisma/client"
 import { FastifyInstance } from "fastify"
-import { NotFound } from "http-errors"
+import { Gone, NotFound } from "http-errors"
+import isbot from "isbot"
 import * as path from "path"
 
 import prisma from "@/config/prisma"
@@ -8,11 +10,10 @@ import backcompat from "@/routes/backcompat"
 
 import api from "./api"
 
-async function routes(fastify: FastifyInstance): Promise<void> {
-  void fastify.register(api, { prefix: "/api" })
+async function routes (fastify: FastifyInstance): Promise<void> {
+  await fastify.register(api, { prefix: "/api" })
   void fastify.register(backcompat)
 
-  console.log(path.join(__dirname, "../../static"))
   // Handle static files
   void fastify.register(fastifyStatic, {
     root: path.join(__dirname, "../../static"),
@@ -21,14 +22,35 @@ async function routes(fastify: FastifyInstance): Promise<void> {
 
   fastify.get("*", async (request, reply) => {
     const url = `${request.hostname}${request.url}`
-    const linkData = await prisma.link.findUnique({
+    const bot = isbot(request.headers["User-Agent"])
+    const linkData = await prisma.link.update({
       where: {
         id: url
+      },
+      data: {
+        visits: {
+          increment: bot ? 0 : 1
+        },
+        scrapes: {
+          increment: bot ? 1 : 0
+        }
+      },
+      include: {
+        comment: true
       }
     })
 
     if (linkData === null) {
       throw new NotFound("Link not found")
+    }
+
+    if (linkData.status === LinkStatus.DISABLED) {
+      let reply = "This link is no longer available."
+      if (linkData.comment !== null) {
+        reply += `\nOperator comments: ${linkData.comment.text}`
+      }
+      throw new Gone(reply)
+
     }
 
     void reply.redirect(linkData.destination)
