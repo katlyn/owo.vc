345fe8fbc2da4eb03a0f113476f3118fae292a0e
Katlyn Lorimer <katlyn@katlyn.dev>
2023-03-28 23:33:29 -0800

    feat: debounce duplicate link creation

--- src/util/shorten.ts
+++ src/util/shorten.ts
@@ -1,14 +1,40 @@
+import { ShortenMethods } from "@prisma/client"
 import { BadRequest, InternalServerError } from "http-errors"
 
 import prisma from "@/config/prisma"
 import generators from "@/generators"
 import { GenerateOptionsType } from "@/routes/api/v2/link"
 
-const shorten = async (options: GenerateOptionsType) => {
+const methodToEnum: Record<GenerateOptionsType["generator"], ShortenMethods> = {
+  owo: ShortenMethods.OWO_VC,
+  zws: ShortenMethods.ZWS,
+  gay: ShortenMethods.GAY,
+  sketchy: ShortenMethods.SKETCHY
+}
 
+const shorten = async (options: GenerateOptionsType) => {
   const url = new URL(options.link)
   if ([ "owo.vc", "owo.gay", "0x6f776f2e7663.net" ].find(d => url.hostname.includes(d)) !== void 0) {
     throw new BadRequest("Refusing to recursively shorten link")
+  }
+
+  // Check to see if the link has been shortened recently
+  const existing = await prisma.link.findFirst({
+    where: {
+      destination: options.link,
+      method: methodToEnum[options.generator],
+      createdAt: {
+        // Created in the last five minutes
+        gte: new Date(Date.now() - 5 * 60 * 1e3)
+      }
+    },
+    orderBy: {
+      createdAt: "desc"
+    }
+  })
+
+  if (existing != null) {
+    return existing
   }
 
   const id = generators[options.generator]()
@@ -18,6 +44,7 @@ const shorten = async (options: GenerateOptionsType) => {
       data: {
         id,
         destination: options.link,
+        method: methodToEnum[options.generator],
         preventScrape: options.preventScrape,
         owoify: options.owoify
       }
