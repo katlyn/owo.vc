356df530f43763dbad2f509aff8cf2b779bf9e6a
Katlyn Lorimer <katlyn@katlyn.dev>
2023-11-18 23:13:02 -0900

    chore: temporarily disable link shortening

--- src/routes/api/v2/index.ts
+++ src/routes/api/v2/index.ts
@@ -1,9 +1,13 @@
 import { FastifyInstance } from "fastify"
 
 import link from "@/routes/api/v2/link"
+import { ServiceUnavailable } from "http-errors"
 
 async function v2 (fastify: FastifyInstance): Promise<void> {
-  void fastify.register(link, { prefix: "/link" })
+  fastify.all("*", async () => {
+    throw new ServiceUnavailable("Service temporarily unavailable due to extended maintenance.")
+  })
+  // void fastify.register(link, { prefix: "/link" })
 }
 
 export default v2
