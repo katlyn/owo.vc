2e638b6acb39d1f640af63f360d3e0ba383aad81
Katlyn Lorimer <katlyn@katlyn.dev>
2025-03-17 17:02:33 -0800

    fix: only allow api on main domain

--- src/routes/api/v2/index.ts
+++ src/routes/api/v2/index.ts
@@ -1,8 +1,18 @@
 import { FastifyInstance } from "fastify"
+import { Forbidden } from "http-errors"
 
+import env from "@/config/env"
 import link from "@/routes/api/v2/link"
 
 async function v2 (fastify: FastifyInstance): Promise<void> {
+  fastify.addHook("onRequest", (request, reply, done) => {
+    if (request.hostname !== env.domain) {
+      throw new Forbidden()
+    }
+    console.log(request.hostname)
+    done()
+  })
+
   void fastify.register(link, { prefix: "/link" })
 }
 
